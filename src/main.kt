99a29f42cde978859d7617fb71f08930b3ba3736
rm4 <ramap664@gmail.com>
2022-03-03 22:13:37 +0700

    ini first commit

---
+++ src/main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    try {
+        print("Masukkan angka pertama: ")
+        val angka1 = readLine()!!.toDouble()
+        print("Masukkan angka kedua: ")
+        val angka2 = readLine()!!.toDouble()
+        print("Masukkan aksi operasi - Tambah(a), Kurang(b), Perkalian(c), Pembagian (d): ")
+        when (readLine()) {
+            "a" -> {
+               //fungsi tambah
+            }
+            "b" -> {
+               //fungsi kurang
+            }
+            "c" -> {
+               //fungsi perkalian
+            }
+            "d" -> {
+               //fungsi pembagian
+            }
+            else -> {
+                print("Operasi yang dimasukkan tidak valid")
+            }
+        }
+    } catch (e: Exception){
+        print("Angka yang dimasukkan tidak valid")
+    }
+}
